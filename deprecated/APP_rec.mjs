bc3f0a24e61782646be6042f0bd63a5f28701e95
Renat G <74renat@gmail.com>
2024-05-25 15:36:56 +0500

    добавил рандома в js

--- deprecated/APP_rec.mjs
+++ deprecated/APP_rec.mjs
@@ -1,18 +1,23 @@
 import { createServer } from 'node:http';
 import { appendFile } from 'node:fs/promises';
 import { format } from 'node:util';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const Math = require("C:/Users/Renat/AppData/Roaming/npm/node_modules/mathjs")
+function getRandom(min, max) { return Math.random() * (max - min) + min; }
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const delay = 3000;
+// @todo get from ENV
+const hostname = '127.0.0.1'; // '192.168.8.242';
+const port = 3001;
+const dirname = 'app1/'; // '/home/user/'; 
 
-const dirname = '/home/user'; // @todo get from ENV
+const delay = 3000; // getRandom(500, 5000);
+
 function loglog(d) {
   console.log(d)
-  appendFile(dirname + '/debug.log', format(d) + '\n');
+  appendFile(dirname + 'debug.log', format(d) + '\n');
 };
 
-// function getRandom(min, max) { return random() * (max - min) + min; }
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
@@ -20,7 +25,7 @@ const server = createServer((req, res) => {
   const timeoutScheduled = Date.now();
   setTimeout(() => {
     const res_delay = Date.now() - timeoutScheduled;
-
+    
     res.end('Hello World!\n');
     loglog(`${res_delay} ms processing request`);
   }, delay);
